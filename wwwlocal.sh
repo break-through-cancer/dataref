b48ce82290eb920aba4dd4c3484230a5763ec2ed
Michael S. Noble <mnoble@broadinstitute.org>
2023-03-04 10:46:00 -0500

    initial version

---
+++ wwwlocal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Simple script to support local viewing of docs prior to # Git commit,
+# push and deployment.  On a Mac will also auto-open browser window.
+
+if [ ! -d docs ] ; then
+    if [ "`which mkdocs`" = "" ] ; then
+        echo "Please ensure 'mkdocs' utility in \$PATH before attempting build."
+        exit 1
+    fi
+    (cd src ; make)
+fi
+
+cd docs
+python3 -m http.server & 
+
+if [ `uname -s` = 'Darwin' ] ; then 
+    open http://localhost:8000
+fi
